a9230955a6da5a3621c29c03eef0b3d4e4aa4247
raul faria <raul@faria.ch>
2024-01-22 17:54:43 +0100

    example form

--- src/pages/CreateRecord/Dozenten.tsx
+++ src/pages/CreateRecord/Dozenten.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { useForm } from "react-hook-form";
 import "./CreateRecord.css";
 import {
+  Form,
   FormControl,
   FormDescription,
   FormField,
@@ -9,25 +12,48 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  username: z.string().min(3, {
+    message: "Username must be at least 3 characters long.",
+  }),
+});
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values);
+}
 
 function CreateDozent() {
-  const form = useForm();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
   return (
     <div>
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>This is your public display name.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 }
