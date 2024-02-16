211ec87508fdcc1d57191d20c09dc74c2a876acf
raul faria <raul@faria.ch>
2024-02-16 14:34:30 +0100

    fixed issue with dozent form schema

--- src/lib/schemas/Dozent.ts
+++ src/lib/schemas/Dozent.ts
@@ -18,35 +18,22 @@ const dozentFormSchema = z.object({
     }),
   strasse: z
     .string()
-    .min(1, {
-      message: 'Straße muss mindestens 1 Zeichen lang sein.',
-    })
+    .max(255, 'Darf maximal 255 Zeichen lang sein.')
     .optional(),
   plz: z
     .string({
       invalid_type_error: 'Muss 4 Zeichen lang sein.',
     })
     .regex(plzRegex, 'Muss 4 zeichen lang sein.')
-    .min(4, {
-      message: 'Muss 4 Zeichen lang sein.',
-    })
-    .max(4, {
-      message: 'Muss 4 Zeichen lang sein.',
-    })
     .optional(),
-  ort: z
-    .string()
-    .min(1, {
-      message: 'Ort muss mindestens 1 Zeichen lang sein.',
-    })
-    .optional(),
+  ort: z.string().max(255, 'Darf maximal 255 Zeichen lang sein.').optional(),
   // nr_land is a picker (constraint),
   nr_land: z.string().regex(numberRegex).optional(),
   // geschlecht is a picker [m, w, d],
   geschlecht: z.enum(['m', 'w', 'd']).optional(),
   telefon: z.string().regex(phoneRegex, 'Ungültige Telefonnummer.').optional(),
   handy: z.string().regex(phoneRegex, 'Ungültige Handynummer').optional(),
-  email: z.string().min(1).email('Muss eine gültige E-Mail sein.').optional(),
+  email: z.string().email('Muss eine gültige E-Mail sein.').optional(),
   // birthate is a date picker,
   birthdate: z
     .string()
