35ea20097fe4dbd0febc466258d0e6abf3166f55
raul faria <raul@faria.ch>
2024-02-16 09:39:52 +0100

    delete for dozenten working

--- src/pages/DataTable/DataTableDozenten.tsx
+++ src/pages/DataTable/DataTableDozenten.tsx
@@ -22,14 +22,34 @@ import { router } from '@/router';
 function DataTableDozenten() {
   const TABLE_NAME = 'dozenten';
   useQueryClient();
-  const { data, isPending } = useQuery({
+  // eslint-disable-next-line prefer-const
+  let { data, isPending, refetch } = useQuery({
     queryKey: ['dozenten'],
     queryFn: getDozenten,
+    initialData: [],
   });
-
+  if (!JSON.stringify(data || {}).startsWith('[') || data === undefined) {
+    refetch();
+    data = [
+      {
+        id: '1',
+        vorname: 'Max',
+        nachname: 'Mustermann',
+        email: 'max@mustermann.com',
+        strasse: 'Musterstrasse 1',
+        plz: '12345',
+        ort: 'Musterstadt',
+        nr_land: '',
+        geschlecht: 'm',
+        telefon: '123456789',
+        handy: '987654321',
+        birthdate: '01.01.1970',
+      },
+    ];
+  }
   return (
     <div>
-      <div className='mb-3'>
+      <div className="mb-3">
         <NavLink to={`/${TABLE_NAME}/create`}>
           <TooltipProvider>
             <Tooltip>
@@ -41,22 +61,22 @@ function DataTableDozenten() {
           </TooltipProvider>
         </NavLink>
       </div>
-      <div className='table bg-white'>
+      <div className="table bg-white">
         <h1>Dozenten</h1>
         <Table>
           <TableHeader>
-            <TableHead className='text-black'>ID</TableHead>
-            <TableHead className='text-black'>Vorname</TableHead>
-            <TableHead className='text-black'>Nachname</TableHead>
-            <TableHead className='text-black'>Email</TableHead>
+            <TableHead className="text-black">ID</TableHead>
+            <TableHead className="text-black">Vorname</TableHead>
+            <TableHead className="text-black">Nachname</TableHead>
+            <TableHead className="text-black">Email</TableHead>
           </TableHeader>
           <TableBody>
             {isPending
               ? Array.from({ length: 4 }).map((_, index) => (
                   <TableRow key={index}>
                     {Array.from({ length: 4 }).map((_, colIndex) => (
-                      <TableCell className='text-left' key={colIndex}>
-                        <Skeleton className='w-full h-[25px] mb-2' />
+                      <TableCell className="text-left" key={colIndex}>
+                        <Skeleton className="w-full h-[25px] mb-2" />
                       </TableCell>
                     ))}
                   </TableRow>
@@ -69,12 +89,12 @@ function DataTableDozenten() {
                         pathname: `/${TABLE_NAME}/${row.id}`,
                       })
                     }
-                    className='cursor-pointer'
+                    className="cursor-pointer"
                   >
-                    <TableCell className='text-left'>{row.id}</TableCell>
-                    <TableCell className='text-left'>{row.vorname}</TableCell>
-                    <TableCell className='text-left'>{row.nachname}</TableCell>
-                    <TableCell className='text-left'>{row.email}</TableCell>
+                    <TableCell className="text-left">{row.id}</TableCell>
+                    <TableCell className="text-left">{row.vorname}</TableCell>
+                    <TableCell className="text-left">{row.nachname}</TableCell>
+                    <TableCell className="text-left">{row.email}</TableCell>
                   </TableRow>
                 ))}
           </TableBody>
